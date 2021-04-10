deff1f13d323174cb814312b0d57f07b8eaf1f6f
VoobaryYanis <79707080+VoobaryYanis@users.noreply.github.com>
2021-04-10 17:08:09 +0200

    Add files via upload

---
+++ [Serv]/papope/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Voobary Yanis'
+description 'PAPOPE ?'
+version '0.0.1'
+
+-- What to run
+client_scripts {
+    'client.lua'
+}
+server_script 'server.lua'
+
